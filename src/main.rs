76b5d1c2ad1f35cd20695f847504312f3d2bbd74
Fuwn <contact@fuwn.me>
2022-03-30 03:48:13 -0700

    format(main): rustfmt

--- src/main.rs
+++ src/main.rs
@@ -48,10 +48,16 @@ use yarte::Template;
 static DATABASE: SyncLazy<Mutex<PickleDb>> = SyncLazy::new(|| {
   Mutex::new({
     if std::fs::File::open(".locus/locus.db").is_ok() {
-      PickleDb::load_json(".locus/locus.db", pickledb::PickleDbDumpPolicy::AutoDump)
-        .unwrap()
+      PickleDb::load_json(
+        ".locus/locus.db",
+        pickledb::PickleDbDumpPolicy::AutoDump,
+      )
+      .unwrap()
     } else {
-      PickleDb::new_json(".locus/locus.db", pickledb::PickleDbDumpPolicy::AutoDump)
+      PickleDb::new_json(
+        ".locus/locus.db",
+        pickledb::PickleDbDumpPolicy::AutoDump,
+      )
     }
   })
 });
